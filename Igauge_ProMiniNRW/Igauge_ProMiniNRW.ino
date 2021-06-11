6fcea9682164535916dc3f3c5532755096b9d7c2
mantis-indonesia <47913785+mantis-indonesia@users.noreply.github.com>
2021-06-12 02:56:05 +0700

    Update Igauge_ProMiniNRW.ino

--- Igauge_ProMiniNRW/Igauge_ProMiniNRW.ino
+++ Igauge_ProMiniNRW/Igauge_ProMiniNRW.ino
@@ -1,5 +1,7 @@
-/* I-GAUGE Pro Mini 2019 5V
-   Instalasi NRW
+/* I-GAUGE Unit Instalasi 2021
+    Kegiatan IUWASH - SECO
+    BEKERJA SAMA DENGAN PDAM DEPOK
+
 */
 
 //LIBRARY
@@ -7,85 +9,36 @@
 #include <avr/sleep.h>
 #include <DS3232RTC.h>        // https://github.com/JChristensen/DS3232RTC
 #include <LowPower.h>
-#include <SoftwareSerial.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADS1015.h>
 #include <Wire.h>
 //custom library
 #include "config.h"
 
-SoftwareSerial serial1(6, 7); // RX, TX
+#define promini 1
 
-//ADS1115
-Adafruit_ADS1115 ads(ads_addr);
+#ifdef promini
+#include <SoftwareSerial.h>
+SoftwareSerial gsm(6, 7); // RX, TX
+#else
+#define gsm Serial1
+#endif
 
-//GLOBAL VARIABLE
-time_t t, alarmTime;
+//parameter waktu
+time_t Waktu, alarmTime;
 byte pembagiWaktu = 60 / interval;
 const time_t ALARM_INTERVAL(interval * 60);  // alarm interval in seconds
 
-boolean ada = 0;
-int i, kode, tahun;
-char karakter;
-byte indeksA, indeksB, indeksC;
-byte bulan, hari, jam, menit, detik;
-unsigned long reads = 0; //pressure
-unsigned long reads1 = 0;
-unsigned long awal, akhir, start;
-float tekanan, volt;
-String kalimat, network, APN, USER, PWD, operators, result;
 
 void setup() {
   //initialisation
-  Serial.begin(baud);  // Serial USB
-  pinMode(13, OUTPUT);
-  pinMode(wakeupPin, INPUT_PULLUP);
-  
+  inisialisasi();
+
   //WELCOME SCREEN
-  Serial.println(F("\r\nGSM IGAUGE PRO MINI 5V 2019"));
+  Serial.println(F("\r\nGSM IGAUGE PRO MINI 5V 2021"));
   Serial.print(F("Unit = "));
   Serial.println(ID);
 
-  //CEK ADS1115
-  Serial.println(F("Cek ADS1115"));
-  Serial.print(F("ADS1115 = "));
-  Wire.begin();
-  Wire.beginTransmission(ads_addr);
-  indeksA = Wire.endTransmission();
-  if (indeksA == 0) {
-    Serial.println("ADS1115 ada");
-    ada = 1;
-    Serial.println(ada);
-  }
-  
-  Serial.println(ada);
-  Wire.end();
-
   //CEK RTC
-  Serial.println(F("Cek RTC"));
-  Wire.begin();
-  Wire.beginTransmission(rtc_addr);
-  indeksA = Wire.endTransmission();
-  if (indeksA == 0) {
-    Serial.println(F("RTC ada"));
-  }
-  if (indeksA != 0) {
-    Serial.println(F("RTC Error"));
-    digitalWrite(13, HIGH);
-    while (1);
-  }
-  Wire.end();
-  setSyncProvider(RTC.get); 
-
-  //ATUR PIN YANG TIDAK DIGUNAKAN SEBAGAI OUTPUT LOW
-  Serial.println(F("Atur pin digital sebagai output LOW"));
-  Serial.flush();
-  for (i = 3; i <= 13; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
-    if (i == 5) i = 9;
-  }
-
+  cekRTC();
 
   // disable unused pin function
   Serial.println(F("Matikan semua fungsi peripheral"));
@@ -103,9 +56,9 @@ void setup() {
     Serial.flush();
     off();
     while (1) {
-      digitalWrite(13, HIGH);
+      digitalWrite(Bled, HIGH);
       LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
-      digitalWrite(13, LOW);
+      digitalWrite(Bled, LOW);
       LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
     }
   }
@@ -119,10 +72,10 @@ void setup() {
   s_on();
   Serial.println(F("\r\nInisialisasi serial GSM"));
   Serial.flush();
-  serial1.begin(baud);  // SIM900A
+
 
   //waiting 15 second for GSM warm up
-  for (indeksA = 1; indeksA <= 15; indeksA++) {
+  for (indeksA = 1; indeksA <= 2; indeksA++) {
     s_on();
     Serial.print(indeksA);
     Serial.println(F(" detik"));
@@ -132,6 +85,11 @@ void setup() {
   s_on();
   Serial.println(F("Cek AT Command"));
   ceksim(); // Cek GSM modul apakah terhubung
+  //Pilih Band
+  Serial.println(F("Pilih Band"));
+  gsm.println(F("AT+CBAND=\"ALL_BAND\""));
+  readSerial(15000);
+
   Serial.println(F("Registrasi SIM"));
   regsim(); // registrasi GSM modul ke network
 
@@ -139,44 +97,46 @@ void setup() {
   on();
   Serial.println(F("Atur Waktu Interupsi.\r\n I2C set di 400K")) ;
   Wire.setClock(400000) ;
+  waktu(); // ambil data waktu dari RTC network
+  /*
+    if (timeStatus() != timeSet) {
+      Serial.println(F("Tidak bisa sinkronisasi dengan RTC. Atur menggunakan GSM"));
+      waktu(); // ambil data waktu dari RTC network
 
-  if (timeStatus() != timeSet) {
-    Serial.println(F("Tidak bisa sinkronisasi dengan RTC. Atur menggunakan GSM"));
-    waktu(); // ambil data waktu dari RTC network
+    }
+    else  {
+      Serial.println(F("Waktu RTC OK"));
 
-  }
-  else  {
-    Serial.println(F("Waktu RTC OK"));
+    }
+  */
 
-  }
-  setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  //setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  Serial.println("gsm tidur");
   GSMsleep(); //GSM modul tidur
   on();
-  
 
   clearRTC();
-
   Serial.print(F("waktu = "));
   cekwaktu();
+  kerja();
 }
 
 void loop() {
   sleepNow(); // Enter sleep
   
   if ( RTC.alarm(ALARM_1) )  {
+    kerja();
+  }
+  
+}
+
+void kerja() {
   Serial.println("Saatnya bekerja");
-  /*
+
   // turn on GSM & Pressure sensor
-  t = RTC.get();
-  tahun = year(t);
-  bulan = month(t);
-  hari = day(t);
-  jam = hour(t);
-  menit = minute(t);
-  detik = second(t);
-  alarmTime = t + ALARM_INTERVAL;
+  ambilWaktu();
   Serial.println(F("Mulai Ambil Data"));
-  printDateTime(t) ;
+  printDateTime(Waktu) ;
 
   //nyalakan GSM jika mati
   if (gsm == 1) {
@@ -184,8 +144,9 @@ void loop() {
     GSMreset();
   }
 
-  ceksim(); // Cek GSM modul apakah terhubung
+  //  ceksim(); // Cek GSM modul apakah terhubung
   regsim(); // registrasi GSM modul ke network
+  gprsComm();
 
   bersihdata();
 
@@ -195,27 +156,21 @@ void loop() {
 
   //burst data 5 seconds
   for (i = 0; i < burst; i++) {
-    digitalWrite(13, HIGH);
-    if (ada == 0)    reads1 = analogRead(pres); //pressure
-    if (ada > 0) reads1 = ads.readADC_SingleEnded(0);
-    if (reads1 < 0) {
-      reads = reads + 0;
-    }
-    else {
-      reads = reads + reads1;
-    }
+    digitalWrite(Bled, HIGH);
+    reads1 = analogRead(pres); //pressure
+    reads = reads + reads1;
     delay(300);
-    digitalWrite(13, LOW);
+    digitalWrite(Bled, LOW);
     delay(700);
   }
 
   //tegangan
-  if (ada == 0)    reads1 = analogRead(tegangan);
-  if (ada > 0) reads1 = ads.readADC_SingleEnded(2);
+  reads1 = analogRead(tegangan);
 
   //KONVERSI TEKANAN
-  volt = (float)reads1 / 1024.00 * 5 ; // nilai voltase dari nilai DN
-  tekanan = ((float)reads / (float)burst) / 1024.00 * 5; // nilai voltase dari nilai DN
+  tekanan = ((float)reads / (float)burst) / 1024.00 * 5.0; // nilai voltase dari nilai DN
+  volt = (float)reads1 / 1024.00 * 5.0 ; // nilai voltase dari nilai DN
+
   Serial.print(F("TEGANGAN SENSOR = "));
   Serial.println(tekanan);
   tekanan = (3.00 * tekanan - 1.5) + float(offset);
@@ -234,19 +189,18 @@ void loop() {
 
   //TURN OFF GSM MODULE
   GSMsleep();
-*/
+
   //atur waktu tidur hingga waktu pengambilan data berikutnya
   on();
-  alarmTime = t + ALARM_INTERVAL;
+  alarmTime = Waktu + ALARM_INTERVAL;
   // set the alarm
   Serial.print(F("Tidur hingga "));
   Serial.print(hour(alarmTime));
   Serial.print(":");
   Serial.println(minute(alarmTime));
   Serial.flush();
- // set the alarm
-    RTC.setAlarm(ALM1_MATCH_HOURS, 0, minute(alarmTime), hour(alarmTime), 0);
-}
+  // set the alarm
+  RTC.setAlarm(ALM1_MATCH_HOURS, 0, minute(alarmTime), hour(alarmTime), 0);
 }
 
 void bersihdata() {
@@ -257,12 +211,13 @@ void sleepNow () {
   Serial.println("Entering sleep");
   delay(100);
   noInterrupts (); // Disable interrupts before entering sleep
-  attachInterrupt (digitalPinToInterrupt(wakeupPin), wake, FALLING);  // Wake on falling edge of D3
+  attachInterrupt (digitalPinToInterrupt(wakeupPin), wake, LOW);  // Wake on falling edge of D2
   interrupts (); // Enable interrupts to ensure next instruction is executed
+  delay(100);
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
 
 
 void wake() {
-detachInterrupt(digitalPinToInterrupt(wakeupPin));
+  detachInterrupt(digitalPinToInterrupt(wakeupPin));
 }

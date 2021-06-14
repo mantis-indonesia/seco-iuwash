c811d0110609447a8f9c80db7625890ce2b836f4
mantis-indonesia <47913785+mantis-indonesia@users.noreply.github.com>
2021-06-14 23:37:48 +0700

    Update config.h

--- Unit_ToT/config.h
+++ Unit_ToT/config.h
@@ -1,13 +1,13 @@
-//#define debug
+#define debug
 //VARIABLES
 float Lat=0.123456;
 float Long=106.123456;
 const byte ID = 11; // unit ID
 #define burst 3 // second
-#define interval 2 // menit
+#define interval 1 // menit
 float offset = 0.0;
 char apn[]="internet";
-byte indeksB = 15; // waktu tunggu GSM
+byte indeksB = 2; // waktu tunggu GSM
 
 #define pres            A1 //channel
 #define led      3

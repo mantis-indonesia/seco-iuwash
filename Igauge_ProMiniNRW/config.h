26e71dd49e3a4f5ef0a9cb1928abe674bdb9e5f1
mantis-indonesia <47913785+mantis-indonesia@users.noreply.github.com>
2021-07-18 22:30:06 +0700

    Update config.h

--- Igauge_ProMiniNRW/config.h
+++ Igauge_ProMiniNRW/config.h
@@ -1,4 +1,10 @@
+//s1 : tekanan, s2: level, s3: terserah, b: baterai
+// nilai lain kalau tidak ada maka diberi -1
+
 #define debug
+String noHP="+6285958553254";
+float batasBawah=5.6;
+float batasAtas=6.0;
 
 //boolean ada = 0;
 int i, kode, tahun;
@@ -11,14 +17,35 @@ unsigned long awal, akhir, start;
 float tekanan, volt;
 String kalimat;
 char apn[]="internet";
+char sTime[]="2021-07-13T15:15:55.000Z";
 String result, json;
 
 
 //VARIABLES
-float Lat=0.123456;
-float Long=106.123456;
+//float Lat=-6.569142;
+//float Long=106.786768;
+float Lat=-6.561963;
+float Long=106.724292;
 
-const byte ID = 11; // unit ID
+//const byte ID = 11; // unit ID
+//const char * ID = "DEP_J_GTA#01"; // unit ID
+//const char * ID = "DEP_P_CLKP#01"; // unit ID
+//const char * ID = "DEP_P_TD#01"; // unit ID pompa taman duta KP 1
+//const char * ID = "DEP_P_TD#02"; // unit ID
+//const char * ID = "DEP_B_SKTN#01"; // unit ID
+//const char * ID = "DEP_J_MTD#01"; // unit ID
+//const char * ID = "DEP_J_PSM#01"; // unit ID
+//const char * ID = "DEP_P_RAN#01"; // unit ID
+//const char * ID = "DEP_P_WFI#01"; // unit ID
+//const char * ID = "DEMO#01"; // unit ID
+//const char * ID = "DEMO#2"; // unit ID
+//const char * ID = "DEMO#3"; // unit ID
+//const char * ID = "DEMO#4"; // unit ID
+//const char * ID = "DEMO#6"; // unit ID
+//const char * ID = "DEMO#8"; // unit ID
+//const char * ID = "DEMO#9"; // unit ID
+//const char * ID = "DEMO#10"; // unit ID
+const char * ID = "DEMO#13"; // unit ID
 #define burst 3 // second
 #define interval 2 // menit
 float offset = 0.0;
